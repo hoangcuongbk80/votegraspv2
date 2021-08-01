dfd04563996c3833bb49b58022f0a3fd9a11a2ad
hoangcuongbk80 <hoangcuongbk80@gmail.com>
2021-08-01 09:50:18 +0700

    predict

--- models/dump_helper.py
+++ models/dump_helper.py
@@ -16,6 +16,35 @@ def softmax(x):
     probs /= np.sum(probs, axis=len(shape)-1, keepdims=True)
     return probs
 
+def batch_viewpoint_params_to_matrix(batch_towards, batch_angle):
+    '''
+    **Input:**
+
+    - towards: numpy array towards vectors with shape (n, 3).
+
+    - angle: numpy array of in-plane rotations (n, ).
+
+    **Output:**
+
+    - numpy array of the rotation matrix with shape (n, 3, 3).
+    '''
+    axis_x = batch_towards
+    ones = np.ones(axis_x.shape[0], dtype=axis_x.dtype)
+    zeros = np.zeros(axis_x.shape[0], dtype=axis_x.dtype)
+    axis_y = np.stack([-axis_x[:,1], axis_x[:,0], zeros], axis=-1)
+    mask_y = (np.linalg.norm(axis_y, axis=-1) == 0)
+    axis_y[mask_y] = np.array([0, 1, 0])
+    axis_x = axis_x / np.linalg.norm(axis_x, axis=-1, keepdims=True)
+    axis_y = axis_y / np.linalg.norm(axis_y, axis=-1, keepdims=True)
+    axis_z = np.cross(axis_x, axis_y)
+    sin = np.sin(batch_angle)
+    cos = np.cos(batch_angle)
+    R1 = np.stack([ones, zeros, zeros, zeros, cos, -sin, zeros, sin, cos], axis=-1)
+    R1 = R1.reshape([-1,3,3])
+    R2 = np.stack([axis_x, axis_y, axis_z], axis=-1)
+    matrix = np.matmul(R2, R1)
+    return matrix.astype(np.float32)
+
 def dump_results(end_points, dump_dir, config, inference_switch=False):
 
     if not os.path.exists(dump_dir):
@@ -42,6 +71,11 @@ def dump_results(end_points, dump_dir, config, inference_switch=False):
     pred_quality = end_points['quality'].detach().cpu().numpy() # B,num_proposal
     pred_width = end_points['width'].detach().cpu().numpy() # B,num_proposal
 
+    print("pred_angle_class: ", pred_angle_class)
+    print("pred_viewpoint_class: ", pred_viewpoint_class)
+
+    batch_viewpoint_params_to_matrix(pred_angle_class, pred_viewpoint_class)
+
     # OTHERS
     #pred_mask = end_points['pred_mask'] # B,num_proposal
     idx_beg = 0
@@ -64,8 +98,10 @@ def dump_results(end_points, dump_dir, config, inference_switch=False):
         if np.sum(objectness_prob>DUMP_CONF_THRESH)>0:
             pc_util.write_ply(pred_center[i,objectness_prob>DUMP_CONF_THRESH,0:3], os.path.join(dump_dir, '%06d_confident_proposal_pc.ply'%(idx_beg+i)))
 
+
+
         # Dump predicted grasps
-        if np.sum(objectness_prob>DUMP_CONF_THRESH)>0:
+        """ if np.sum(objectness_prob>DUMP_CONF_THRESH)>0:
             num_proposal = pred_center.shape[1]
             for j in range(num_proposal):
                 grasp = config.param2grasp(pred_center[i,j,0:3], pred_viewpoint_class[i,j],
@@ -76,6 +112,6 @@ def dump_results(end_points, dump_dir, config, inference_switch=False):
                     str_num = '{:.6f}'.format(ite)
                     f.write(str_num)
                     f.write(' ')
-                f.write("\n")
+                f.write("\n") """
 
     f.close()

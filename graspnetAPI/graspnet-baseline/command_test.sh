eddb064464ca5b719baf5a98c964763b47f07bc9
hoangcuongbk80 <hoangcuongbk80@gmail.com>
2021-08-25 22:34:45 +0700

    testing

--- graspnetAPI/graspnet-baseline/command_test.sh
+++ graspnetAPI/graspnet-baseline/command_test.sh
@@ -1,4 +1,4 @@
 #CUDA_VISIBLE_DEVICES=0 python test.py --dump_dir logs/dump_rs --checkpoint_path logs/log_rs/checkpoint.tar --camera realsense --dataset_root /data/Benchmark/graspnet
 # CUDA_VISIBLE_DEVICES=0 python test.py --dump_dir logs/dump_kn --checkpoint_path logs/log_kn/checkpoint.tar --camera kinect --dataset_root /data/Benchmark/graspnet
 
-CUDA_VISIBLE_DEVICES=0 python test.py --dump_dir /media/hoang/HD-PZFU3/datasets/graspnet/predictions --checkpoint_path /media/hoang/HD-PZFU3/datasets/graspnet/checkpoint.tar --camera kinect --dataset_root /media/hoang/HD-PZFU3/datasets/graspnet
+CUDA_VISIBLE_DEVICES=0 python3 test.py --dump_dir /media/hoang/HD-PZFU3/datasets/graspnet/predictions --checkpoint_path /media/hoang/HD-PZFU3/datasets/graspnet/checkpoint.tar --camera kinect --dataset_root /media/hoang/HD-PZFU3/datasets/graspnet
